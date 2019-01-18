ecea4262a3a7256cb825c4130364dc576a214b58
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-18 02:53:08 -0500

    Add files via upload

---
+++ basicphp/controllers/pages/welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * In the controller file, you can handle and process variables,
+ * classes and functions; use if-elseif statements; handle your
+ * database connection like PDO abstraction layer; and load/require
+ * files. The variables can then be used in the view file.
+ */
+
+// Show header and menu
+require '../template/header.php';
+require '../template/menu.php';
+
+// Set variable in controller
+$param1 = url_value(1);
+
+// Render welcome view
+// Use variable inside view using native PHP templating
+require '../views/pages/welcome.php';
+
+// Show footer
+require '../template/footer.php';
+
+?>
