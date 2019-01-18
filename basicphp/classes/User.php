143584ac45032e4886e1ddcc1dba5d71ed6e1bb5
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-18 11:35:59 -0500

    Add files via upload

---
+++ basicphp/classes/User.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Sample user-defined class
+ */
+
+class User
+
+{
+
+	public static function show()
+
+	{
+
+		echo 'This is a method from the user-defined class.';
+
+	}
+
+}
+
+?>
