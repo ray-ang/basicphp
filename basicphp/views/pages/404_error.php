c9dd042b9c44b2b523d50d0b46504f61f06a6c60
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-18 02:54:54 -0500

    Add files via upload

---
+++ basicphp/views/pages/404_error.php
@@ -0,0 +1,3 @@
+<h3>This is the Error Page.</h3>
+<?php if (isset($error_message)) echo 'ERROR: <strong>' . $error_message . '</strong>' ; ?>
+<p>The error message will appear above if there's an error.</p>
