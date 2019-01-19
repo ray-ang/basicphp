bd3a54b21f02c3288ada0d855d14df05e706123f
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-19 01:46:10 -0500

    Add files via upload

--- basicphp/views/routes/sample_route.php
+++ basicphp/views/routes/sample_route.php
@@ -1,6 +1,12 @@
 <p>This is a sample URL route.</p>
-<p>Variables can be used after defining them in the controller file.</p>
+<p>Variables can be used to render view after defining them in the controller file.</p>
 <p>Templating is done using native PHP templating.</p>
+<h4>Passing Data from Controller</h4>
+<?php foreach($age as $x => $x_value): ?>
+The name is <?= $x ?> and the age is <?= $x_value ?>.
+<br>
+<?php endforeach ?>
+<h4>Using URL substring as parameter.</h4>
 <?php if (! empty($param1)): ?>
 <p>The first paramter is <?= $param1 ?>.</p>
 <?php endif ?>
