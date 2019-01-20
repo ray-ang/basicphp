045c388c13fb7f90aea1fdf25fb05a8ef5bf82e8
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-20 09:19:25 -0500

    Render error page View using $data = compact()

--- basicphp/views/pages/error.php
+++ basicphp/views/pages/error.php
@@ -1,3 +1,3 @@
 <h3>This is the Error Page.</h3>
-<?php if (isset($error_message)) echo 'ERROR: <strong>' . $error_message . '</strong>' ; ?>
+<?php if (isset($data['error_message'])) echo 'ERROR: <strong>' . $data['error_message'] . '</strong>' ; ?>
 <p>The error message will appear above if there's an error.</p>
