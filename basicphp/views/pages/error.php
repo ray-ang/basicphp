e11700f877c9ba94eaf21ca718cedc8e57225d21
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-01-21 05:42:58 -0500

    Delete error.php

--- basicphp/views/pages/error.php
+++
@@ -1,3 +0,0 @@
-<h3>This is the Error Page.</h3>
-<?php if (isset($data['error_message'])) echo 'ERROR: <strong>' . $data['error_message'] . '</strong>' ; ?>
-<p>The error message will appear above if there's an error.</p>
