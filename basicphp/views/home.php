369db51c2721039b86344d86735da6254f3ccf9b
Ray-Ang <46805160+Ray-Ang@users.noreply.github.com>
2019-02-07 11:45:12 -0500

    Updated homepage

--- basicphp/views/home.php
+++ basicphp/views/home.php
@@ -3,8 +3,8 @@
       <div class="row">
         <div class="col-lg-12 text-center">
           <h1 class="mt-5">BasicPHP Starter Application</h1>
-          <p>Welcome to the BasicPHP Micro-Framework Starter Application!</p>
-          <p>This starter application can be used as a foundation in building web applications using the BasicPHP framework.</p>
+          <p>Welcome to the BasicPHP Nano-Framework Starter Application!</p>
+          <p>This starter application can be used as a foundation in building web applications using the BasicPHP framework, or embedded in the framework of choice.</p>
         </div>
       </div>
     </div>
