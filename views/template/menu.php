10a532dfbccac476bf98aae7e5cc6a304b90274f
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-08-24 20:57:30 -0400

    Changed url_value( ) in menu.php

--- views/template/menu.php
+++ views/template/menu.php
@@ -5,23 +5,23 @@
       <div class="container">
         <a class="navbar-brand" href="<?php echo BASE_URL; ?>">BasicPHP Starter</a>
           <ul class="nav nav-pills">
-            <li class="nav-item <?php if (url_value(0) == '') echo 'active'; ?>">
+            <li class="nav-item <?php if (url_value(1) == '') echo 'active'; ?>">
               <a class="nav-link" href="<?php echo BASE_URL; ?>">Home</a>
             </li>
-            <li class="nav-item <?php if (url_value(0) == 'welcome') echo 'active'; ?>">
+            <li class="nav-item <?php if (url_value(1) == 'welcome') echo 'active'; ?>">
               <a class="nav-link" href="<?php echo BASE_URL; ?>welcome">Welcome</a>
             </li>
-            <li class="nav-item <?php if (url_value(0) == 'request') echo 'active'; ?>">
+            <li class="nav-item <?php if (url_value(1) == 'request') echo 'active'; ?>">
               <a class="nav-link" href="<?php echo BASE_URL; ?>request">Request</a>
             </li>
-            <li class="dropdown <?php if (url_value(0) == 'post') echo 'active'; ?>">
+            <li class="dropdown <?php if (url_value(1) == 'post') echo 'active'; ?>">
 	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Posts <span class="caret"></span></a>
 	          <ul class="dropdown-menu">
 	            <li><a href="<?php echo BASE_URL; ?>post/list">All Posts</a></li>
 	            <li><a href="<?php echo BASE_URL; ?>post/add">Add Post</a></li>
 	          </ul>
 	        </li>
-			<li class="nav-item <?php if (url_value(0) == 'sample' && url_value(2) == 'route') echo 'active'; ?>">
+			<li class="nav-item <?php if (url_value(1) == 'sample' && url_value(2) == 'route') echo 'active'; ?>">
 			  <a class="nav-link" href="<?php echo BASE_URL; ?>sample/route">Sample</a>
 			</li>
             <li class="nav-item">
