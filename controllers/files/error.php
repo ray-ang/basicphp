08fbce4a8ed725a943786c7ea8e20cc424274386
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-10-24 13:24:15 -0400

    Delete error.php

--- controllers/files/error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * In the controller file, you can handle and process variables,
- * classes and functions; use if-elseif statements; load models, and
- * include files. The variables can then be used in the view file.
- */
-
-View::page('error');
