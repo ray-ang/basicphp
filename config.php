d2e17597408be8af10ba4b1cd0d5ede17658e8e8
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-11-02 01:21:27 -0400

    Added ( . ) and ( _ ) in whitelisted URI chars

--- config.php
+++ config.php
@@ -73,7 +73,7 @@ define('FIREWALL_ON', TRUE);
 // List of allowed IP addresses in an array
 define('ALLOWED_IP_ADDR', ['::1']);
 // Set URI Whitelisted Characters
-define('URI_WHITELISTED', '\w\/\-\?\=\&');
+define('URI_WHITELISTED', '\w\/\.\-\_\?\=\&');
 // Blacklisted $_POST and post body characters. '\' blacklisted by default.
 define('POST_BLACKLISTED', '\<\>\;\#\\$');
 
