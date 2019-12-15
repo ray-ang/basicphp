dca49f12ed2170dacd93e01962bbfb48d574de63
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-12-15 05:23:35 -0500

    Version-based encryption; iterations based on NIST

--- functions.php
+++ functions.php
@@ -368,23 +368,33 @@ function csrf_token()
 function encrypt($plaintext)
 {
 
-	// Cipher method to CBC with 256-bit key
-	$cipher = CIPHER_METHOD;
-	// Salt for encryption key
-	$salt_key = random_bytes(16);
-	// Derive encryption key
-	$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt_key, 1000, 20);
-	// Salt for HMAC key
-	$salt_hmac = random_bytes(16);
-	// Derive HMAC key
-	$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt_hmac, 1000, 20);
-	// Initialization vector
-	$iv = random_bytes(16);
+	function encrypt_v1($plaintext) {
 
-	$ciphertext = openssl_encrypt($plaintext, $cipher, $key, 0, $iv);
-	$hash = hash_hmac('sha256', $ciphertext, $key_hmac);
+		// Version
+		$version = 'enc-v1';
+		
+			// Cipher method to CBC with 256-bit key
+		$cipher = CIPHER_METHOD;
+		// Salt for encryption key
+		$salt_key = random_bytes(16);
+		// Derive encryption key
+		$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt_key, 10000);
+		// Salt for HMAC key
+		$salt_hmac = random_bytes(16);
+		// Derive HMAC key
+		$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt_hmac, 10000);
+		// Initialization vector
+		$iv = random_bytes(16);
 
-	return base64_encode($ciphertext) . '::' . base64_encode($hash) . '::' . base64_encode($iv) . '::' . base64_encode($salt_key) . '::' . base64_encode($salt_hmac);
+		$ciphertext = openssl_encrypt($plaintext, $cipher, $key, 0, $iv);
+		$hash = hash_hmac('sha256', $ciphertext, $key_hmac);
+
+		return $version . '::' . base64_encode($ciphertext) . '::' . base64_encode($hash) . '::' . base64_encode($iv) . '::' . base64_encode($salt_key) . '::' . base64_encode($salt_hmac);
+
+	}
+
+	/** Version-based Encryption */
+	return encrypt_v1($plaintext);
 
 }
 
@@ -398,32 +408,49 @@ function encrypt($plaintext)
 function decrypt($encrypted)
 {
 
-	// Return empty if $encrypted is not set or empty.
-	if ( ! isset($encrypted) || empty($encrypted) ) { return ''; }
+	function decrypt_v1($encrypted) {
 
-	// Cipher method to CBC with 256-bit key
-	$cipher = CIPHER_METHOD;
+		// Return empty if $encrypted is not set or empty.
+		if ( ! isset($encrypted) || empty($encrypted) ) { return ''; }
 
-	list($ciphertext, $hash, $iv, $salt_key, $salt_hmac) = explode('::', $encrypted);
-	$ciphertext = base64_decode($ciphertext);
-	$hash = base64_decode($hash);
-	$iv = base64_decode($iv);
-	$salt_key = base64_decode($salt_key);
-	$salt_hmac = base64_decode($salt_hmac);
+		// Cipher method to CBC with 256-bit key
+		$cipher = CIPHER_METHOD;
 
-	// Derive encryption key
-	$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt_key, 1000, 20);
-	// Derive HMAC key
-	$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt_hmac, 1000, 20);
+		list($version, $ciphertext, $hash, $iv, $salt_key, $salt_hmac) = explode('::', $encrypted);
+		$version = base64_decode($version);
+		$ciphertext = base64_decode($ciphertext);
+		$hash = base64_decode($hash);
+		$iv = base64_decode($iv);
+		$salt_key = base64_decode($salt_key);
+		$salt_hmac = base64_decode($salt_hmac);
 
-	$digest = hash_hmac('sha256', $ciphertext, $key_hmac);
+		// Derive encryption key
+		$key = hash_pbkdf2('sha256', PASS_PHRASE, $salt_key, 10000);
+		// Derive HMAC key
+		$key_hmac = hash_pbkdf2('sha256', PASS_PHRASE, $salt_hmac, 10000);
 
-	// HMAC authentication
-	if  ( hash_equals($hash, $digest) ) {
-		return openssl_decrypt($ciphertext, $cipher, $key, 0, $iv);
+		$digest = hash_hmac('sha256', $ciphertext, $key_hmac);
+
+		// HMAC authentication
+		if  ( hash_equals($hash, $digest) ) {
+			return openssl_decrypt($ciphertext, $cipher, $key, 0, $iv);
+			}
+		else {
+			exit ('<strong>Warning: </strong>Please verify authenticity of ciphertext.');
 		}
-	else {
-		exit ('<strong>Warning: </strong>Please verify authenticity of ciphertext.');
+
+	}
+
+	$version = explode('::', $encrypted)[0];
+
+	/** Version-based Decryption */
+	// Return $encrypted if no encryption detected.
+	switch ($version) {
+		case 'enc-v1':
+			return decrypt_v1($encrypted);
+			break;
+		default:
+			return $encrypted;
 	}
 
 }
