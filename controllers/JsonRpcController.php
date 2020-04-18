11f08bb1c9e50eed3a2fce99996f3958a83aa046
ray-ang <46805160+ray-ang@users.noreply.github.com>
2020-04-18 10:27:41 -0400

    Sample controller for JSON-RPC

---
+++ controllers/JsonRpcController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Controller for JSON-RPC protocol.
+ */
+
+class JsonRpcController
+{
+
+	public function index()
+	{
+
+		json_rpc(); // Enable application for JSON-RPC v2.0 protocol
+
+	}
+
+}
