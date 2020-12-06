d0cfce3756564c2392e9b416c93ab0307c50d6dd
ray-ang <46805160+ray-ang@users.noreply.github.com>
2020-12-06 02:53:05 -0500

    Upgraded to Bootstrap 4

--- sample-site/views/template/footer.php
+++ sample-site/views/template/footer.php
@@ -1,6 +1,2 @@
-	<!-- jQuery library -->
-	<script src="<?php echo BASE_URL;?>assets/jquery.min.js"></script>
-	<!-- Latest compiled JavaScript -->
-	<script src="<?php echo BASE_URL;?>assets/bootstrap.min.js"></script>
 </body>
 </html>
--- sample-site/views/template/menu.php
+++ sample-site/views/template/menu.php
@@ -5,24 +5,26 @@
       <div class="container">
         <a class="navbar-brand" href="<?php echo BASE_URL; ?>">BasicPHP Starter</a>
           <ul class="nav nav-pills">
-            <li class="nav-item <?php if (Basic::segment(1) == '') echo 'active'; ?>">
-              <a class="nav-link" href="<?php echo BASE_URL; ?>">Home</a>
+            <li class="nav-item">
+              <a class="nav-link <?php if (Basic::segment(1) == '') echo 'active'; ?>" href="<?php echo BASE_URL; ?>">Home</a>
             </li>
-            <li class="nav-item <?php if (Basic::segment(1) == 'encryption') echo 'active'; ?>">
-              <a class="nav-link" href="<?php echo BASE_URL; ?>encryption">Encryption</a>
+            <li class="nav-item">
+              <a class="nav-link <?php if (Basic::segment(1) == 'encryption') echo 'active'; ?>" href="<?php echo BASE_URL; ?>encryption">Encryption</a>
             </li>
-            <li class="nav-item <?php if (Basic::segment(1) == 'request') echo 'active'; ?>">
-              <a class="nav-link" href="<?php echo BASE_URL; ?>request">Request</a>
+            <li class="nav-item">
+              <a class="nav-link <?php if (Basic::segment(1) == 'request') echo 'active'; ?>" href="<?php echo BASE_URL; ?>request">Request</a>
             </li>
-            <li class="dropdown <?php if (Basic::segment(1) == 'post') echo 'active'; ?>">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="TRUE" aria-expanded="FALSE">Posts <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo BASE_URL; ?>post/list">All Posts</a></li>
-                <li><a href="<?php echo BASE_URL; ?>post/add">Add Post</a></li>
-              </ul>
+            <li class="dropdown">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                Posts
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="<?php echo BASE_URL; ?>post/list">All Posts</a>
+                <a class="dropdown-item" href="<?php echo BASE_URL; ?>post/add">Add Post</a>
+              </div>
             </li>
-            <li class="nav-item <?php if (Basic::segment(1) == 'sample' && Basic::segment(2) == 'route') echo 'active'; ?>">
-              <a class="nav-link" href="<?php echo BASE_URL; ?>sample/route">Sample</a>
+            <li class="nav-item">
+              <a class="nav-link <?php if (Basic::segment(1) == 'sample' && Basic::segment(2) == 'route') echo 'active'; ?>" href="<?php echo BASE_URL; ?>sample/route">Sample</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="<?php echo BASE_URL; ?>login">Log In</a>
