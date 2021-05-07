8f78208b22e75cedb4cb0e8cf3084fa972744575
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-05-07 04:32:31 -0400

    v0.9.7 to v0.9.8

--- Basic.php
+++ Basic.php
@@ -13,7 +13,7 @@
  *          - include/require Basic.php.
  *
  * @package  BasicPHP
- * @version  v0.9.7
+ * @version  v0.9.8
  * @author   Raymund John Ang <raymund@open-nis.org>
  * @license  MIT License
  */
