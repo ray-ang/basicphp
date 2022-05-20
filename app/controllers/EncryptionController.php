3658fe33116c8a01e066c33a143a997fb4a0da2a
ray-ang <46805160+ray-ang@users.noreply.github.com>
2022-05-20 19:04:10 -0400

    Used encv2
    Uses basic AES ECB

--- app/controllers/EncryptionController.php
+++ app/controllers/EncryptionController.php
@@ -7,8 +7,8 @@ class EncryptionController
 	{
 		$page_title = 'Data Encryption';
 		$plaintext = '{"username":"user","role":"admin"}';
-		$encrypted = Basic::encrypt($plaintext, PASS_PHRASE);
-		$decrypted = Basic::decrypt($encrypted, PASS_PHRASE);
+		$encrypted = Basic::encrypt($plaintext, PASS_PHRASE, 'encv2');
+		$decrypted = Basic::decrypt($encrypted, PASS_PHRASE, 'encv2');
 
 		Basic::view('encryption', compact('page_title', 'plaintext', 'encrypted', 'decrypted'));
 	}
