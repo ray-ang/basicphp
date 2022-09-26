6dee468cbf875e81aa699610dcc4d020973ade33
ray-ang <46805160+ray-ang@users.noreply.github.com>
2022-09-25 20:37:43 -0400

    Added token verification

--- app/controllers/HttpRpcController.php
+++ app/controllers/HttpRpcController.php
@@ -3,15 +3,30 @@
  * HTTP-RPC Controller
  * 
  * Endpoint: api/httprpc (Basic::setHttpRpc())
+ * Parameters: ?action=HttpRpc.calcDouble&num=5&token=12345
+ * Result: 10
  */
 
-class HttpRpcController
+class HttpRpcController extends Basic
 {
+	private $token = '12345';
+
+	public function __construct()
+	{
+		$this->_verify_access();
+	}
+
+	public function _verify_access()
+	{
+		if (empty($_GET['token']) || $_GET['token'] !== $this->token) {
+			self::apiResponse(401, 'Invalid token.');
+		}
+	}
 
 	public function calcSingle()
 	{
 		$num = $_GET['num'];
-		if (! is_numeric($num)) Basic::apiResponse(400, 'Error: Num parameter should be a number.');
+		if (! is_numeric($num)) self::apiResponse(400, 'Error: Num parameter should be a number.');
 		$res = $num;
 		
 		return $res;
@@ -20,7 +35,7 @@ class HttpRpcController
 	public function calcDouble()
 	{
 		$num = $_GET['num'];
-		if (! is_numeric($num)) Basic::apiResponse(400, 'Error: Num parameter should be a number.');
+		if (! is_numeric($num)) self::apiResponse(400, 'Error: Num parameter should be a number.');
 		$res = $num * 2;
 		
 		return $res;
